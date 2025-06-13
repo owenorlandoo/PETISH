6565723f742ca4590a67b203932fafc74a8604e6
Marshanda <marshanda01@student.ciputra.ac.id>
2025-06-14 00:13:39 +0700

    tambah shelter

--- pet-certificate-ui/src/App.js
+++ pet-certificate-ui/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RoleSelection from "./pages/RoleSelection";
 // import UserDashboard from "./pages/UserDashboard";
-// import ShelterDashboard from "./pages/ShelterDashboard";
+import ShelterDashboard from "./pages/ShelterDashboard";
 // import VetDashboard from "./pages/VetDashboard";       // ← Tambahkan ini
 import OwnerDashboard from "./pages/OwnerDashboard";   // ← Tambahkan ini
 
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<RoleSelection />} />
         src="/Petish_Logo.png"
-        {/* <Route path="/shelter" element={<ShelterDashboard />} /> */}
+        <Route path="/shelter" element={<ShelterDashboard />} />
         {/* <Route path="/vet" element={<VetDashboard />} /> */}
         <Route path="/owner" element={<OwnerDashboard />} />
         {/* <Route path="/user" element={<UserDashboard />} /> */}
